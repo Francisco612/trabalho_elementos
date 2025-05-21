fc2464cedf9e9d8eb0cfb5b4b9718a3beaf350a1
Francisco612 <francisco.b.sousa@ubi.pt>
2025-05-21 16:34:50 +0100

    .

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ df_merged = pd.merge(ab, ds, on=["01. Ano", "Território"], how="inner")
 
 #alterar o nome da coluna modalidade de ensino e taxa de abandono
 df_merged = df_merged.rename(columns={"04. Filtro 1_x": "Modalidade de ensino"})
-df_merged = df_merged.rename(columns={"09. Valor_x": "Taxa de retenção e desistência no ensino secunário, em todos os anos de escolaridade"})
+df_merged = df_merged.rename(columns={"09. Valor_x": "Taxa de retenção e desistência no ensino secundário, em todos os anos de escolaridade"})
 
 #intrevalo do ano em estudo desde 2009 até 2023
 df_merged["Ano"] = pd.to_numeric(df_merged.pop("01. Ano"), errors="coerce")
@@ -34,7 +34,7 @@ df_merged = df_merged[df_merged["05. Filtro 2_x"] == "Total"].drop(columns=["05.
 df_merged = df_merged.drop(columns=["06. Filtro 3_x", "07. Escala_x","08. Símbolo_y","07. Escala_y","06. Filtro 3_y","05. Filtro 2_y", "08. Símbolo_x"])
 
 #remonear a coluna do desemprego para menores de 25 anos
-df_merged = df_merged.rename(columns={"09. Valor_y": "Desemprego registado nos centros de emprego(para menores de 25 anos)"})
+df_merged = df_merged.rename(columns={"09. Valor_y": "Desemprego registado nos centros de emprego (para menores de 25 anos)"})
 
 #ordenar as colunas
 df_merged = df_merged[["Ano", "Território", "Modalidade de ensino", "Taxa de retenção e desistência no ensino secunário, em todos os anos de escolaridade", "Desemprego registado nos centros de emprego(para menores de 25 anos)"]]
